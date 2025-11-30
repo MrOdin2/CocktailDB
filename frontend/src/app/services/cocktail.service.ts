02703828044a9cee8b036981b3af93d80f601079
MrOdin <fabian.klimkeit001@stud.fh-dortmund.de>
2025-11-30 16:04:00 +0100

    add caching for Ingredients and Cocktails

--- frontend/src/app/services/cocktail.service.ts
+++ frontend/src/app/services/cocktail.service.ts
@@ -1,48 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Cocktail } from '../models/models';
-import { environment } from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, shareReplay, tap} from 'rxjs';
+import {Cocktail} from '../models/models';
+import {environment} from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CocktailService {
-  private baseUrl = `${environment.apiUrl}/cocktails`;
+    private baseUrl = `${environment.apiUrl}/cocktails`;
+    private cache$?: Observable<Cocktail[]>;
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  getAll(): Observable<Cocktail[]> {
-    return this.http.get<Cocktail[]>(this.baseUrl, { withCredentials: true });
-  }
+    getAll(forceRefresh: boolean = false): Observable<Cocktail[]> {
+        if (!this.cache$ || forceRefresh) {
+        return this.http.get<Cocktail[]>(this.baseUrl, {withCredentials: true}).pipe(
+            shareReplay({bufferSize: 1, refCount: true})
+        );}
+        return this.cache$;
+    }
 
-  getById(id: number): Observable<Cocktail> {
-    return this.http.get<Cocktail>(`${this.baseUrl}/${id}`, { withCredentials: true });
-  }
+    getById(id: number): Observable<Cocktail> {
+        return this.http.get<Cocktail>(`${this.baseUrl}/${id}`, {withCredentials: true});
+    }
 
-  create(cocktail: Cocktail): Observable<Cocktail> {
-    return this.http.post<Cocktail>(this.baseUrl, cocktail, { withCredentials: true });
-  }
+    create(cocktail: Cocktail): Observable<Cocktail> {
+        return this.http.post<Cocktail>(this.baseUrl, cocktail, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  update(id: number, cocktail: Cocktail): Observable<Cocktail> {
-    return this.http.put<Cocktail>(`${this.baseUrl}/${id}`, cocktail, { withCredentials: true });
-  }
+    update(id: number, cocktail: Cocktail): Observable<Cocktail> {
+        return this.http.put<Cocktail>(`${this.baseUrl}/${id}`, cocktail, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`, { withCredentials: true });
-  }
+    delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  getAvailable(): Observable<Cocktail[]> {
-    return this.http.get<Cocktail[]>(`${this.baseUrl}/available`);
-  }
+    getAvailable(): Observable<Cocktail[]> {
+        return this.http.get<Cocktail[]>(`${this.baseUrl}/available`);
+    }
 
-  search(name?: string, spirit?: string, tags?: string[]): Observable<Cocktail[]> {
-    let params: any = {};
-    if (name) params.name = name;
-    if (spirit) params.spirit = spirit;
-    if (tags && tags.length > 0) params.tags = tags;
+    search(name?: string, spirit?: string, tags?: string[]): Observable<Cocktail[]> {
+        let params: any = {};
+        if (name) params.name = name;
+        if (spirit) params.spirit = spirit;
+        if (tags && tags.length > 0) params.tags = tags;
 
-    return this.http.get<Cocktail[]>(`${this.baseUrl}/search`, { params, withCredentials: true });
-  }
+        return this.http.get<Cocktail[]>(`${this.baseUrl}/search`, {params, withCredentials: true});
+    }
+
+    invalidateCache(): void {
+        this.cache$ = undefined;
+    }
 }
 
--- frontend/src/app/services/ingredient.service.ts
+++ frontend/src/app/services/ingredient.service.ts
@@ -1,39 +1,56 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Ingredient } from '../models/models';
-import { environment } from '../../environments/environment';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, shareReplay, tap} from 'rxjs';
+import {Ingredient} from '../models/models';
+import {environment} from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class IngredientService {
-  private baseUrl = `${environment.apiUrl}/ingredients`;
+    private baseUrl = `${environment.apiUrl}/ingredients`;
+    private cache$?: Observable<Ingredient[]>;
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  getAll(): Observable<Ingredient[]> {
-    return this.http.get<Ingredient[]>(this.baseUrl, { withCredentials: true });
-  }
+    getAll(forceRefresh: boolean = false): Observable<Ingredient[]> {
+        if (!this.cache$ || forceRefresh) {
+            this.cache$ = this.http.get<Ingredient[]>(this.baseUrl, {withCredentials: true}).pipe(
+                shareReplay({bufferSize: 1, refCount: true})
+            );
+        }
+        return this.cache$;
+    }
 
-  getById(id: number): Observable<Ingredient> {
-    return this.http.get<Ingredient>(`${this.baseUrl}/${id}`, { withCredentials: true });
-  }
+    getById(id: number): Observable<Ingredient> {
+        return this.http.get<Ingredient>(`${this.baseUrl}/${id}`, {withCredentials: true});
+    }
 
-  create(ingredient: Ingredient): Observable<Ingredient> {
-    return this.http.post<Ingredient>(this.baseUrl, ingredient, { withCredentials: true });
-  }
+    create(ingredient: Ingredient): Observable<Ingredient> {
+        return this.http.post<Ingredient>(this.baseUrl, ingredient, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  update(id: number, ingredient: Ingredient): Observable<Ingredient> {
-    return this.http.put<Ingredient>(`${this.baseUrl}/${id}`, ingredient, { withCredentials: true });
-  }
+    update(id: number, ingredient: Ingredient): Observable<Ingredient> {
+        return this.http.put<Ingredient>(`${this.baseUrl}/${id}`, ingredient, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`, { withCredentials: true });
-  }
+    delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`, {withCredentials: true}).pipe(
+            tap(() => this.invalidateCache())
+        );
+    }
 
-  getInStock(): Observable<Ingredient[]> {
-    return this.http.get<Ingredient[]>(`${this.baseUrl}/in-stock`, { withCredentials: true });
-  }
+    getInStock(): Observable<Ingredient[]> {
+        return this.http.get<Ingredient[]>(`${this.baseUrl}/in-stock`, {withCredentials: true});
+    }
+
+    invalidateCache(): void {
+        this.cache$ = undefined;
+    }
 }
 
